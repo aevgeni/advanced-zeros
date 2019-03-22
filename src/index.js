5cda5b7e51ba508bfe54971d927bd6d1f8acba87
AEvgeni <eslilolko@mail.ru>
2019-03-22 22:18:01 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function getZerosCount(number, base) {
+//module.exports = function getZerosCount(number, base) {
+function getZerosCount(number) {  
   // your implementation
+  
+  var z = 0;
+  while(number){
+    number = (number/5)|0;
+    z+=number;
+  }
+  console.log(z);
+  return z;
+  
 }
+
+getZerosCount(16);
