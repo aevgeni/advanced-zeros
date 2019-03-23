bbc57be3b88d08f83cde9a606508f84bb216a184
aevgeni <eslilolko@mail.ru>
2019-03-23 13:58:02 +0300

    zeros test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-//module.exports = function getZerosCount(number, base) {
-function getZerosCount(number) {  
+module.exports = function getZerosCount(number, base) {
+
   // your implementation
   
-  var z = 0;
-  while(number){
-    number = (number/5)|0;
-    z+=number;
+  let reminder;
+  let ZerosCount = number;
+  let localBase = base;
+
+  for (let i = 2; i <= base; i++) {
+    if (localBase % i === 0) {
+      reminder = 0;      
+      while (localBase % i === 0) {
+        localBase = Math.floor(localBase / i);
+        reminder++;
+      };      
+
+      let count = 0;
+      let tempNum = number;
+      while (tempNum / i > 0) {
+        count += Math.floor(tempNum / i);
+        tempNum = Math.floor(tempNum / i);
+      };
+      let timeVar = Math.floor(count / reminder);
+      if (ZerosCount > timeVar) {
+        ZerosCount = timeVar;
+        console.log("number:" + number + " i=" + i + " timeVar=" + timeVar + " base=" + base);
+      }
+    }
+    
   }
-  console.log(z);
-  return z;
+  return ZerosCount;
   
 }
 
-getZerosCount(16);
