b1088199fb14226672b0655c4c0de431855bbe08
AEvgeni <eslilolko@mail.ru>
2019-03-23 20:39:57 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,34 +1,52 @@
 module.exports = function getZerosCount(number, base) {
-
   // your implementation
-  
-  let reminder;
-  let ZerosCount = number;
-  let localBase = base;
 
-  for (let i = 2; i <= base; i++) {
-    if (localBase % i === 0) {
-      reminder = 0;      
-      while (localBase % i === 0) {
-        localBase = Math.floor(localBase / i);
-        reminder++;
-      };      
-
-      let count = 0;
-      let tempNum = number;
-      while (tempNum / i > 0) {
-        count += Math.floor(tempNum / i);
-        tempNum = Math.floor(tempNum / i);
-      };
-      let timeVar = Math.floor(count / reminder);
-      if (ZerosCount > timeVar) {
-        ZerosCount = timeVar;
-        console.log("number:" + number + " i=" + i + " timeVar=" + timeVar + " base=" + base);
+  //собираем массив множителей
+  let arrTemp = [];
+  if (base > 0) {
+    for (let i = 2; i <= base; i++){
+      while (base % i == 0) {
+        arrTemp.push(i);
+        base = base / i;
       }
-    }
-    
+    }    
   }
-  return ZerosCount;
-  
-}
 
+  //массив множителей и степеней множителей 
+  let arrMultiplier = [];
+  let arrDegree = [];  
+  for (let i = 0; 1 <= arrTemp.length; i++) {
+    let degree = 1;
+    let multiplier = arrTemp[0];
+    for (let k = 1; k < arrTemp.length; k++) {
+      if (arrTemp[k] == multiplier) {
+        degree++;
+      }
+    }    
+    //добавляем множитель и его степень в массив
+    arrMultiplier.push(multiplier);
+    arrDegree.push(degree);
+    //удаляем использованный множитель
+    arrTemp = arrTemp.filter(element => element !== multiplier);
+  } 
+                                        
+  let arrZeros = [];
+  for (let i = 0; i < arrMultiplier.length; i++) {
+      let factor = 0;
+      let numbTemp = number;
+        while (numbTemp >= 1) {
+          numbTemp /= arrMultiplier[i];
+          factor += Math.floor(numbTemp);
+        }
+      let c = Math.floor(factor / arrDegree[i]);
+      arrZeros.push(c);
+  }
+
+  let zeros = arrZeros[0];  
+  for (let i = 1; i <= arrZeros.length; i++) {
+    if (arrZeros[i] < zeros) {
+      zeros = arrZeros[i];
+    }
+  }
+  return zeros;
+}
